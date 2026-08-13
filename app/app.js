405d45618b14b422d4ee920a494a5603e39ef359
azamdev <azam@zen.com.my>
2026-08-13 11:32:17 +0800

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from the DevSecOps lab! ' + _.capitalize('scan me'));
+});
+
+server.listen(3000, () => console.log('Running on port 3000'));
